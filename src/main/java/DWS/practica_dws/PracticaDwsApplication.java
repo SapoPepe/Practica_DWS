899d7f764eb532e793defb18f54e44f7045d1161
SapoPepe <gz.adrian.200@gmail.com>
2024-02-21 18:49:07 +0100

    Initial commit

---
+++ src/main/java/DWS/practica_dws/PracticaDwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.practica_dws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaDwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticaDwsApplication.class, args);
+    }
+
+}
