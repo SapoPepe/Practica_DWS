a965b1aaa46b97aed1ac60d98433a2ed901d9b87
SapoPepe <gz.adrian.200@gmail.com>
2024-02-23 11:19:44 +0100

    Creation classes

--- src/main/java/DWS/practica_dws/PracticaDwsApplication.java
+++ src/main/java/DWS/practica_dws/PracticaDwsApplication.java
@@ -1,4 +1,4 @@
-package com.example.practica_dws;
+package DWS.practica_dws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
