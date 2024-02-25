bce5925ef1a7a0cb2704c0dd23af9547a2d64693
jcifr99 <j.cifuentes.2021@alumnos.urjc.es>
2024-02-25 18:39:08 +0100

    Fixing errors in Comments

--- src/main/java/DWS/practica_dws/model/Product.java
+++ src/main/java/DWS/practica_dws/model/Product.java
@@ -1,22 +1,20 @@
 package DWS.practica_dws.model;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Product {
     private long id;
     private double prize;
     private String description;
     private String name;
-    private Map<String, Comment> comments;
+    private List<Comment> comments;
 
 
     public Product(String name, String description, double prize){
         this.name = name;
         this.description = description;
         this.prize = prize;
-        this.comments = new HashMap<>();
+        this.comments = new ArrayList<>();
 
     }
 
@@ -32,18 +30,11 @@ public class Product {
         return this.name;
     }
 
-    public double getPrice() {
-        return prize;
-    }
-    public String getDescription() {
-        return description;
-    }
-
     public void addComment(Comment c){
-        this.comments.put(c.getUser(), c);
+        this.comments.add(c);
     }
 
     public Collection<Comment> getComments(){
-        return this.comments.values();
+        return this.comments;
     }
 }
